ffee143bdcda4518dfd560359b1cf1f8d5f39e1a
Karkulevskiy <mdkarkulevskiy@edu.hse.ru>
2024-04-08 07:08:53 +0300

    Add swagger

--- src/main.go
+++ src/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	swaggerFiles "github.com/swaggo/files"
+	_ "shareen/src/docs"
 
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	router.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	router.Run()
 }
